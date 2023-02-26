d46b1de987598d4ba1eaddd559bcd4a909d8c169
Arthur Cosentino <arthurcose@gmail.com>
2023-02-26 13:16:23 -0500

    Update about

--- pages/about.tsx
+++ pages/about.tsx
@@ -17,16 +17,19 @@ export default function Home() {
       <ul className={styles.thanks_list}>
         <li>Alison Allbright</li>
         <li>Alrysc</li>
+        <li>cantrem404</li>
         <li>CosmicNobab</li>
+        <li>ChordMankey</li>
         <li>Dawn Elaine</li>
         <li>D3str0y3d255</li>
         <li>DJRezzed</li>
         <li>Dunstan</li>
+        <li>Ehab2020</li>
         <li>FrozenLake</li>
         <li>Gemini</li>
         <li>Gray Nine</li>
         <li>JamesKing</li>
-        <li>jontheburger</li>
+        <li>JonTheBurger</li>
         <li>KayThree</li>
         <li>Keristero</li>
         <li>Konstinople</li>
@@ -36,14 +39,17 @@ export default function Home() {
         <li>Mars</li>
         <li>Mint</li>
         <li>Mithalan</li>
+        <li>nickblock</li>
         <li>OuroYisus</li>
         <li>Pion</li>
         <li>Salad</li>
         <li>Shale</li>
+        <li>svenevs</li>
         <li>theWildSushii</li>
         <li>Pheelbert</li>
         <li>PlayerZero</li>
         <li>Poly</li>
+        <li>Pyredrid</li>
         <li>Weenie</li>
         <li>Zeek</li>
       </ul>

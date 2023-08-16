f351f2fea897b1aee3464ea05ca69475b1691fab
Arthur Cosentino <arthurcose@gmail.com>
2023-08-16 09:58:10 -0400

    Add a progress bar for long page loads

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,18 @@ import { AppContextProvider } from "@/components/context";
 import Layout from "@/components/layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { PagesProgressBar as ProgressBar } from "next-nprogress-bar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppContextProvider>
+      <ProgressBar
+        height="3px"
+        color="white"
+        options={{ showSpinner: false }}
+        delay={100}
+        shallowRouting
+      />
       <Layout>
         <Component {...pageProps} />
       </Layout>

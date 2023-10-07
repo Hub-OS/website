04077cbcdc5b5acc16131f2ca36aed849fc02484
Arthur Cosentino <arthurcose@gmail.com>
2023-10-07 12:59:18 -0400

    Add requestVoid utility function

--- types/request.ts
+++ types/request.ts
@@ -16,3 +16,20 @@ export async function requestJSON(
     return Err(String(err));
   }
 }
+
+export async function requestVoid(
+  uri: string,
+  init?: RequestInit | undefined
+): Promise<Result<void, string>> {
+  try {
+    const res = await fetch(uri, init);
+
+    if (res.status != 200) {
+      return Err(await res.text());
+    }
+
+    return Ok(undefined);
+  } catch (err) {
+    return Err(String(err));
+  }
+}

b9e4c59cad4f5d5fc2057b914879d583518354f8
Arthur Cosentino <arthurcose@gmail.com>
2024-05-15 13:25:51 -0400

    Remove compression from full previews

--- pages/index.tsx
+++ pages/index.tsx
@@ -74,7 +74,7 @@ export default function Home() {
             {"<"}
           </button>
 
-          <Image src={images[imageIndex]} alt="" quality={100} priority />
+          <Image src={images[imageIndex]} alt="" unoptimized priority />
 
           <button
             aria-label="Next Image"

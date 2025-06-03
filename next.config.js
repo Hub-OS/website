e596e92bd6fb5082a9eb8d09a596cb1250d4283f
Arthur Cosentino <arthurcose@gmail.com>
2025-06-03 12:28:23 -0400

    Skip type checking in production

--- next.config.js
+++ next.config.js
@@ -3,9 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   images: { remotePatterns: [{ hostname: "cdn.discordapp.com" }] },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 

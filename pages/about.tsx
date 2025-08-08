aeceda7531112e460d350f4d4449fd13f44b1c0b
Arthur Cosentino <arthurcose@gmail.com>
2025-08-08 13:59:36 -0400

    Add dedicated credits page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,7 @@
+import React from "react";
+import Head from "next/head";
 import Link from "next/link";
 import styles from "@/styles/About.module.css";
-import Head from "next/head";
 
 export default function About() {
   return (
@@ -11,8 +12,15 @@ export default function About() {
 
       <div>
         <p>
-          Hub OS is an online battle experience that takes place on
-          interconnected servers, or Hubs.
+          Hub OS is an open-source Mega Man Battle Network fan project and
+          online battle experience that takes place on interconnected servers,
+          or Hubs.
+        </p>
+
+        <br />
+
+        <p>
+          <Link href="/credits">CREDITS!</Link>
         </p>
 
         <br />
@@ -86,50 +94,6 @@ export default function About() {
 
         <br />
 
-        <h2>Special Thanks</h2>
-
-        <ul className={styles.thanks_list}>
-          <li>Abigail Allbright</li>
-          <li>Alrysc</li>
-          <li>cantrem404</li>
-          <li>CosmicNobab</li>
-          <li>ChordMankey</li>
-          <li>Dawn Elaine</li>
-          <li>D3str0y3d255</li>
-          <li>DJRezzed</li>
-          <li>Dunstan</li>
-          <li>Ehab2020</li>
-          <li>FrozenLake</li>
-          <li>Gemini</li>
-          <li>Gray Nine</li>
-          <li>Gwyneth Hestin</li>
-          <li>JamesKing</li>
-          <li>JonTheBurger</li>
-          <li>KayThree</li>
-          <li>Keristero</li>
-          <li>Konstinople</li>
-          <li>Kuri</li>
-          <li>kyqurikan</li>
-          <li>Loui</li>
-          <li>Mars</li>
-          <li>Mint</li>
-          <li>Mithalan</li>
-          <li>Mx. Claris Glennwood</li>
-          <li>nickblock</li>
-          <li>OuroYisus</li>
-          <li>Pion</li>
-          <li>Salad</li>
-          <li>Shale</li>
-          <li>svenevs</li>
-          <li>theWildSushii</li>
-          <li>Pheelbert</li>
-          <li>PlayerZero</li>
-          <li>Poly</li>
-          <li>Pyredrid</li>
-          <li>Weenie</li>
-          <li>Zeek</li>
-        </ul>
-        <br />
         <span>
           This website was made with{" "}
           <Link href="/licenses">open source software</Link>
---
+++ pages/credits.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+import classNames from "classnames";
+import styles from "@/styles/Credits.module.css";
+
+const CREDITS_LINKS: { [key: string]: React.JSX.Element } = {
+  "Mondo.sp": (
+    <Link href="https://www.youtube.com/channel/UC70QE2DBtzkG_W4_NWLvW2A">
+      YouTube
+    </Link>
+  ),
+  OpenNetBattle: (
+    <Link href="https://github.com/theMaverickProgrammer/opennetbattle">
+      GitHub
+    </Link>
+  ),
+};
+
+const CREDITS_LIST: [string, string[]][] = [
+  ["Music", ["Mondo.sp"]],
+  ["Sound Design", ["DJRezzed"]],
+  ["Graphics", ["Konst", "Salad"]],
+  ["User Experience", ["Gray Nine", "KayThree"]],
+  [
+    "Testers",
+    ["DeltaFiend", "Jack", "KayThree", "Kyqurikan", "PlayerZero", "Void.ExE"],
+  ],
+  ["Technical Design", ["Dawn Elaine", "JamesKing", "Mars"]],
+  ["Programmers", ["Dawn Elaine", "JamesKing", "Konst"]],
+  ["Core Team", ["Dawn Elaine", "Konst", "Mars"]],
+  [
+    "Special Thanks",
+    [
+      "Abigail Allbright",
+      "Alrysc",
+      "cantrem404",
+      "CosmicNobab",
+      "ChordMankey",
+      "Dunstan",
+      "Ehab2020",
+      "FrozenLake",
+      "Gemini",
+      "Gwyneth Hestin",
+      "JonTheBurger",
+      "Keristero",
+      "Kuri",
+      "Loui",
+      "Mint",
+      "Mithalan",
+      "Mx. Claris Glennwood",
+      "nickblock",
+      "OuroYisus",
+      "Pion",
+      "Pheelbert",
+      "Poly",
+      "Pyredrid",
+      "Shale",
+      "svenevs",
+      "theWildSushii",
+      "Weenie",
+      "Zeek",
+    ],
+  ],
+  ["Our Roots", ["OpenNetBattle"]],
+  ["For Inspiring Us", ["Mega Man Battle Network", "Capcom"]],
+];
+
+export default function Credits() {
+  return (
+    <>
+      <Head>
+        <title>Credits - Hub OS</title>
+      </Head>
+
+      <div className={styles.credits}>
+        <h1>Hub OS Credits</h1>
+
+        {CREDITS_LIST.map(([section_name, list]) => (
+          <React.Fragment key={section_name}>
+            <h2>{section_name}</h2>
+
+            <ul className={classNames(list.length > 12 && styles.long_list)}>
+              {list.map((name) => {
+                const link = CREDITS_LINKS[name];
+
+                return (
+                  <li key={name}>
+                    {name}
+                    {link && (
+                      <>
+                        {" ("}
+                        {link}
+                        {")"}
+                      </>
+                    )}
+                  </li>
+                );
+              })}
+            </ul>
+
+            <br />
+          </React.Fragment>
+        ))}
+      </div>
+    </>
+  );
+}

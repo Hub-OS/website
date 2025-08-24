11410b75b3f08af72610ceab3718eee37bf8055d
Arthur Cosentino <arthurcose@gmail.com>
2025-08-24 00:54:43 -0400

    Fix missing uploader data on web

--- pages/mods/[id].tsx
+++ pages/mods/[id].tsx
@@ -222,15 +222,20 @@ export async function getServerSideProps(context: NextPageContext) {
 
   async function requestUploader() {
     if (props.meta) {
+      console.log("a");
       const uploaderId = props.meta.creator;
       const uri = `${process.env.NEXT_PUBLIC_HOST!}/api/users/${uploaderId}`;
 
       const uploaderResult = await requestJSON(uri);
 
       if (uploaderResult.ok) {
+        console.log("b");
         props.uploader = uploaderResult.value;
+      } else {
+        console.log("c");
       }
     }
+    console.log("d");
   }
 
   async function requestEditPermission() {
@@ -250,11 +255,14 @@ export async function getServerSideProps(context: NextPageContext) {
     }
   }
 
-  await Promise.all([
-    requestMeta(),
-    requestUploader(),
-    requestEditPermission(),
-  ]);
+  const promises = [requestEditPermission()];
+
+  // remaining promises require props.meta
+  await requestMeta();
+
+  promises.push(requestUploader());
+
+  await Promise.all(promises);
 
   return {
     props,
